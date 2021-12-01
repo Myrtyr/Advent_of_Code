d1ba430b384986bdf1900e280c3d829734ec399f
Myrtyr <myrtevbelkom@gmail.com>
2021-12-01 09:51:05 +0100

    Solutions for day1

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,4 +1,4 @@
-with open('data/input_day1.txt', 'r') as f:
+with open("2021/data/input_day1.txt", "r") as f:
     depths_raw = f.readlines()
 
 counter = 0
@@ -7,5 +7,20 @@ for depth in depths_raw:
     next = int(depth)
     if next > start:
         counter += 1
+    start = next
+print(counter)
 
+counter = 0
+start = int(depths_raw[0])
+mid = int(depths_raw[1])
+end = int(depths_raw[2])
+for depth in depths_raw:
+    next = int(depth)
+    old = start + mid + end
+    new = old - start + next
+    if new > old:
+        counter += 1
+    start = mid
+    mid = end
+    end = next
 print(counter)

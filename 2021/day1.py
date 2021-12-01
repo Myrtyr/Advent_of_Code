0dbf09b7e55b91a1d791237e618328106db8a98f
Myrtyr <myrtevbelkom@gmail.com>
2021-12-01 09:40:15 +0100

    new try

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,1 +1,11 @@
-print('hello world')
+with open('data/input_day1.txt', 'r') as f:
+    depths_raw = f.readlines()
+
+counter = 0
+start = int(depths_raw[0])
+for depth in depths_raw:
+    next = int(depth)
+    if next > start:
+        counter += 1
+
+print(counter)

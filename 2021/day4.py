2a2e8613c19e33b8aee9f4759394d66bd671617f
Myrtyr <myrtevbelkom@gmail.com>
2021-12-04 10:23:49 +0100

    Solution for day 4 (1 and 2) of 2021

--- 2021/day4.py
+++ 2021/day4.py
@@ -1,42 +1,46 @@
+import numpy as np
+
 with open ("2021/data/input_day4.txt", "r") as f:
     order = f.readline()[:-1].split(",")
     f.readline()
     bingo_cards = f.read()[:-1].split("\n\n")
-print(order)
+
 all_bingo_cards = []
 for bingo_card in bingo_cards:
     rows = bingo_card.split("\n")
-    all_bingo_cards.append([])
+    to_append = []
     for row in rows:
+        to_append.append([])
         numbers = row.split()
-        all_bingo_cards[-1].append(numbers)
+        for number in numbers:
+            to_append[-1].append(int(number))
+    all_bingo_cards.append(np.array(to_append))
+
+
+def finish_game(bingo_card):
+    remaining = [int(item) for items in bingo_card for item in items]
+    start = 0
+    for elt in remaining:
+        start += elt
+    score = int(number) * start
+    print("There's a winner, with a score of: ", score)
+    return score
 
 for number in order:
-    for bingo_card in all_bingo_cards:
-        print(bingo_card)
+    for index in range(len(all_bingo_cards)):
+        bingo_card = all_bingo_cards[index]
         for row_index in range(len(bingo_card)):
             row = bingo_card[row_index]
             for col_index in range(len(row)):
-                if row[col_index] == number:
+                if row[col_index] == int(number):
                     bingo_card[row_index][col_index] = 0
         for col_index in range(len(row)):
+            if sum(bingo_card[:,col_index]) == 0:
+                finish_game(bingo_card)
+                all_bingo_cards[index] = 100*np.ones((5,5))
 
         for row_index in range(len(bingo_card)):
-            row = bingo_card[row_index]
-
-            if not row:
-                bingo_card.remove([])
-                remaining = [int(item) for items in bingo_card for item in items]
-                print(remaining, number)
-                start = 0
-                for elt in remaining:
-                    start += elt
-                print("There's a winner, with a score of: ", int(number)*start)
-                break
-        else:
-            continue
-        break
-    else:
-        continue
-    break
+            if sum(bingo_card[row_index,:]) == 0:
+                finish_game(bingo_card)
+                all_bingo_cards[index] = 100*np.ones((5,5))
 
